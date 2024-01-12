8180cd6a3cefb68bd93b2be778905e59a44cd3ae
WafflesItemManager <67392804+WafflesItemManager@users.noreply.github.com>
2024-01-12 16:03:48 +0100

    added formatter script

---
+++ format.py
@@ -0,0 +1,10 @@
+import os
+
+from trainer import Trainer, TrainerArgs
+from TTS.demos.xtts_ft_demo.utils.formatter import format_audio_list
+
+def main():
+  format_audio_list()
+
+if __name__ == "__main__":
+    main()
